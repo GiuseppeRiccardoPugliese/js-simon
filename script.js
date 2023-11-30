4eac84a5259db92533b69bf37b59b40e8d8933f1
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-11-30 19:28:24 +0100

    Start btn for play the game, TIME function & CHECK for num

--- script.js
+++ script.js
@@ -12,15 +12,19 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 //Dopo che sono stati inseriti i 5 numeri, ci viene detto quanti e quali numeri da indovinare sono stati individuati
 
 let arrayNumb = [];
+let arrayToCompare = [];
 const numbToShow = document.getElementById('numbers_show');
 const btnStart = document.getElementById('start');
 const userNumbers = document.getElementById('user_numb');
 const btnCheckUserNum = document.getElementById('check_numb');
+const result = document.getElementById('risultato');
+const numbToCompare = document.getElementById('compare_num');
 let seconds = 30;
 let punteggio = 0;
+let maxTentativi = 5;
 
 
-//Start del timer
+//Start del timer e del gioco
 btnStart.addEventListener('click',
     function () {
         document.getElementById("numbers_show").style.display = "block";
@@ -32,19 +36,31 @@ btnStart.addEventListener('click',
             arrayNumb.push(genRandomNum(1, 100));
             numbToShow.innerHTML = arrayNumb;
         }
+        console.log(arrayNumb);
+
+        //Check dei numeri inseriti dall'utente al mio click
+        btnCheckUserNum.addEventListener('click',
+            function () {
+                let checkNumber = parseInt(userNumbers.value);
+
+                if (!arrayNumb.includes(checkNumber) && maxTentativi > 0) {
+                    maxTentativi--;
+                    console.log("questi sono i tentativi massimi", maxTentativi);
+                } else if (arrayNumb.includes(checkNumber) && maxTentativi > 0) {
+                    punteggio++;
+                    maxTentativi--;
+                    arrayToCompare.push(checkNumber);
+                    console.log("questi sono i tentativi massimi", maxTentativi);
+                }
+                if (maxTentativi === 0) {
+                    result.innerHTML = `Questo e' il tuo punteggio: ${punteggio}`;
+                    numbToCompare.innerHTML = `Hai indovinato questi numeri: ${arrayToCompare}`;
+                }
+            }
+        )
     }
+
 );
-console.log(arrayNumb);
-
-btnCheckUserNum.addEventListener('click',
-    function () {
-        let checkNumber = userNumbers.value;
-        if (arrayNumb.includes(checkNumber)) {
-            punteggio++;
-        }
-    }
-)
-
 
 
 
