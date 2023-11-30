a35e0f90ae26d12e1857e04064671d7dd59a1a45
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-11-30 14:39:57 +0100

    Scomposizione del problema in sotto problemi

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ Visualizzare in pagina 5 numeri casuali.
 Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, i numeri che ha visto precedentemente.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
+
+// 1 visualizzare in pagina 5 numeri CASUALI
+// Dopo aver visualizzato i numeri, parte un timer di 30 seocndi
+// Dopo i 30 secondi, i numeri scompaiono
+//Far inserire all'utente i numeri che ha visto precedentemente
+//Dopo che sono stati inseriti i 5 numeri, ci viene detto quanti e quali numeri da indovinare sono stati individuati
